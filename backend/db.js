2807cb7a6e8b2e0a6686f3c57606698b0cf6c8e8
samriddhidev <shuklasamriddhi1102@gmail.com>
2023-11-18 18:31:30 +0530

    displaying data on home page

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,6 @@ const mongoURI =
   "mongodb://samriddhidev:anmolanmol@ac-wkkicuf-shard-00-00.b4yivxp.mongodb.net:27017,ac-wkkicuf-shard-00-01.b4yivxp.mongodb.net:27017,ac-wkkicuf-shard-00-02.b4yivxp.mongodb.net:27017/vegfood?ssl=true&replicaSet=atlas-8jw43y-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 const connectDB = async () => {
-  let data;
-
   try {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
@@ -15,16 +13,21 @@ const connectDB = async () => {
     console.log("Connected to the database");
 
     const fetched_data = mongoose.connection.db.collection("food_items");
-    data = await fetched_data.find({}).toArray();
-    
+    const data = await fetched_data.find({}).toArray();
+
+    const foodcategory = mongoose.connection.db.collection("foodcategory");
+    try {
+      const catData = await foodcategory.find({}).toArray();
+      global.foodcategory = catData;
+    } catch (error) {
+      console.error("Error fetching foodcategory data", error);
+    }
+
+    global.food_items = data;
   } catch (error) {
     console.error("Error connecting to the database", error);
     // Handle the error here or throw it to be caught by the calling function
     throw error;
-  }
-
-  if (data) {
-    global.food_items = data;
   }
 };
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,73 @@
-import React from "react";
+ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 import Carousel from "../components/Carousel";
 
 export default function Home() {
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    try {
+      let response = await fetch("http://localhost:5000/api/foodData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Data received from the server:", data);
+      setFoodCat(data[0]);
+      setFoodItem(data[1]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Calling loadData...");
+    loadData();
+  }, []);
+
+  console.log("Render, foodCat:", foodCat);
+  console.log("Render, foodItem:", foodItem);
+
+  // Create an array of unique category names
+  const uniqueCategories = [
+    ...new Set(foodItem.map((item) => item.CategoryName)),
+  ];
+
   return (
     <div>
-      <Navbar/>
-      <div><Carousel/></div>
+      <Navbar />
       <div>
-        <div>
-          <Card />
+        <Carousel />
+      </div>
+      <div className="container">
+        {/* Use uniqueCategories to create columns */}
+        <div className="row">
+          {uniqueCategories.map((category) => (
+            <div key={category} className="col-sm">
+              <div className="fs-3 m-3">{category}</div>
+              <hr />
+              {foodItem
+                .filter((item) => item.CategoryName === category)
+                .map((filterItems) => (
+                  <div key={filterItems._id}>
+                    <Card data={filterItems} />
+                  </div>
+                ))}
+            </div>
+          ))}
         </div>
+      </div>
+      <div>
         <Footer />
       </div>
     </div>
